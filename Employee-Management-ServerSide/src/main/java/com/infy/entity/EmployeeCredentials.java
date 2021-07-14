d4f4f549bfa1da9013312822c55131fd179ecf4c
GOPIKA M <GOPIKA M@DESKTOP-DFAT97O>
2021-07-14 16:29:26 +0800

    modified employee credentials class

--- Employee-Management-ServerSide/src/main/java/com/infy/entity/EmployeeCredentials.java
+++ Employee-Management-ServerSide/src/main/java/com/infy/entity/EmployeeCredentials.java
@@ -3,42 +3,31 @@ package com.infy.entity;
 import java.time.LocalDate;
 
 public class EmployeeCredentials {
-private Integer employeeId;
+private Integer emailId;
 private String password;
-private LocalDate joiningDate;
-private LocalDate dateOfBirth;
 
 
-public EmployeeCredentials(Integer employeeId, String password, LocalDate joiningDate, LocalDate dateOfBirth) {
+public EmployeeCredentials(Integer emailId, String password) {
 	super();
-	this.employeeId = employeeId;
+	this.emailId = emailId;
 	this.password = password;
-	this.joiningDate = joiningDate;
-	this.dateOfBirth = dateOfBirth;
 }
-public Integer getEmployeeId() {
-	return employeeId;
+
+
+public Integer getEmailId() {
+	return emailId;
 }
-public void setEmployeeId(Integer employeeId) {
-	this.employeeId = employeeId;
+
+public void setEmailId(Integer emailId) {
+	this.emailId = emailId;
 }
+
 public String getPassword() {
 	return password;
 }
+
 public void setPassword(String password) {
 	this.password = password;
-}
-public LocalDate getJoiningDate() {
-	return joiningDate;
-}
-public void setJoiningDate(LocalDate joiningDate) {
-	this.joiningDate = joiningDate;
-}
-public LocalDate getDateOfBirth() {
-	return dateOfBirth;
-}
-public void setDateOfBirth(LocalDate dateOfBirth) {
-	this.dateOfBirth = dateOfBirth;
 }
 
 }
